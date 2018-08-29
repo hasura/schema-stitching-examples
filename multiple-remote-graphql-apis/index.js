e59c1e714dcab91e97863efc39f8964aee311212
wawhal <rishichandra.wawhal@gmail.com>
2018-08-29 15:31:27 +0530

    update examples

--- multiple-remote-graphql-apis/index.js
+++ multiple-remote-graphql-apis/index.js
@@ -7,14 +7,10 @@ import { HttpLink } from 'apollo-link-http';
 import { ApolloServer } from 'apollo-server';
 import fetch from 'node-fetch';
 
-// secret keys
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN || '96e41cf1d251074283f908674c3d37791f056540';
-
 // graphql API metadata
 const graphqlApis = [
   {
-    uri: 'https://api.github.com/graphql',
-    headers: { 'Authorization': `Bearer ${GITHUB_TOKEN}` }
+    uri: 'https://bazookaand.herokuapp.com/v1alpha1/graphql',
   },
   {
     uri: 'https://schema-stitching-blog.herokuapp.com/v1alpha1/graphql',
@@ -30,7 +26,6 @@ const createRemoteExecutableSchemas = async () => {
   for (const api of graphqlApis) {
     const link = new HttpLink({
       uri: api.uri,
-      headers: api.headers,
       fetch
     });
     const remoteSchema = await introspectSchema(link);
--- new-queries-mutations/index.js
+++ new-queries-mutations/index.js
@@ -1,84 +1,88 @@
-import {
-  makeRemoteExecutableSchema,
-  makeExecutableSchema,
-  introspectSchema,
-  mergeSchemas
-} from 'graphql-tools';
+import { introspectSchema, makeExecutableSchema, makeRemoteExecutableSchema, mergeSchemas } from 'graphql-tools';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloServer } from 'apollo-server';
 import fetch from 'node-fetch';
+import { ApolloServer } from 'apollo-server';
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN || '5940911256dfc2227e5b458606a465a239b350b0';
+const METAWEATHER_API_URL = "https://www.metaweather.com/api/location/";
 
-// Initial counter state
-let count = 0;
-
-// Typedefs for hello world query
-const helloTypeDefs = `
-  type Query {
-    hello: String,
-		count: Int
-  }
-
-  type Mutation {
-		increment_counter: counter_mutation_result
-  }
-
-  type counter_mutation_result {
-    new_count: Int
-  }
-`;
-
-// Resolvers for hello world query
-const helloResolvers = {
-  Query: {
-    hello: (root, args, context) => {
-      return 'Hello world!';
-    },
-    count: (root, args, context) => {
-      return count;
-    }
-  },
-  Mutation: {
-    increment_counter: (root, args, context, info) => {
-      return { "new_count": ++count };
-    }
-  }
-};
-
-// Create executable schema from Github GraphQL API
-const createGithubSchema = async () => {
-  // Create an Apollo link with 'uri' of Github API
+// Create executable schema from user GraphQL API
+const createUserSchema = async () => {
+  // Create an Apollo link with 'uri' of user API
   const link = new HttpLink({
-    uri: `https://api.github.com/graphql`,
-    fetch,
-    headers: {
-      'Authorization': `Bearer ${GITHUB_TOKEN}` 
-    }
+    uri: `https://bazookaand.herokuapp.com/v1alpha1/graphql`,
+    fetch
   });
   // Get remote schema
-  const remoteGithubSchema = await introspectSchema(link);
+  const remoteUserSchema = await introspectSchema(link);
   // Make executable schema
   return makeRemoteExecutableSchema({
-    schema: remoteGithubSchema,
+    schema: remoteUserSchema,
     link
   });
 };
 
-// Merge schemas
+// Pass the city and get the unique woeid for the city
+const getWoeid = (place) => {
+  return fetch(`${METAWEATHER_API_URL}search/?query=${place}`)
+    .then(response => response.json())
+    .then(jsonResponse => jsonResponse[0])
+}
+
+// Use the woeid of the city to get its weather
+const getWeather = (data) => {
+  return fetch(METAWEATHER_API_URL + data.woeid)
+      .then(response => response.json())
+};
+
+const typeDefs = `
+    type CityWeather {
+      temp: String
+      min_temp: String
+      max_temp: String
+      city_name: String!
+      applicable_date: String!
+    }
+    type Query {
+      cityWeather(city_name: String! applicable_date: String): CityWeather
+    }
+  `;
+
+// Resolvers for cityWeather query
+const resolvers = {
+  Query: {
+    cityWeather: (root, args, context, info) => {
+      return getWoeid(args.city_name).then( function(response) {
+        if (!response) {
+          return null;
+        }
+        return getWeather(response).then( function(weather) {
+          if (!weather) {
+            return null;
+          }
+          let consolidated_weather = weather.consolidated_weather;
+          // check for args applicable_date to apply filter
+          consolidated_weather = args.applicable_date ? consolidated_weather.find(item => item.applicable_date === args.applicable_date) : consolidated_weather[0];
+          const respObj = {'temp': consolidated_weather.the_temp.toString(), 'min_temp': consolidated_weather.min_temp.toString(), 'max_temp': consolidated_weather.max_temp.toString(), 'city_name': weather.title, 'applicable_date': consolidated_weather.applicable_date};
+          return respObj;
+        });
+      });
+    }
+  },
+};
+
 const createNewSchema = async () => {
-  // Make and get executable Github Schema
-  const githubExecutableSchema = await createGithubSchema();
-  // Make executable hello-world schema
-  const helloExecutableSchema = makeExecutableSchema({
-    typeDefs: helloTypeDefs,
-    resolvers: helloResolvers
+  // Make and get executable user Schema
+  const userExecutableSchema = await createUserSchema();
+  // Make executable weather schema
+  const weatherExecutableSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers
   });
   // Merge the two schemas
   return mergeSchemas({
     schemas: [
-      githubExecutableSchema,
-      helloExecutableSchema
+      userExecutableSchema,
+      weatherExecutableSchema
     ]
   });
 };
@@ -87,13 +91,11 @@ const runServer = async () => {
   // Get newly merged schema
   const schema = await createNewSchema();
   // start server with the new schema
-  const server = new ApolloServer({
-    schema
-  });
+  const server = new ApolloServer({ schema });
   server.listen().then(({url}) => {
     console.log(`Running at ${url}`);
   });
-};
+}
 
 try {
   runServer();
